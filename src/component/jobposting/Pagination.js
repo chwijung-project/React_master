8932b6283bb6a47467027c5620cf43010fb51499
khe8895 <khe8895@gachon.ac.kr>
2024-02-29 23:31:10 +0900

    Change-Dropdown position

--- src/component/jobposting/Pagination.js
+++ src/component/jobposting/Pagination.js
@@ -1,14 +1,22 @@
+import React, {useEffect, useState} from 'react';
 import './Pagination.css';
 
 
 function Pagination({ currentPage, setCurrentPage, pageInfo, postList }) {
     const pageNumber = [];
+    const limit = 10 ;
+    const [currentPageArray, setCurrentPageArray] = useState([]);
 
     if(pageInfo){
         for(let i = 1; i <= pageInfo.pageEnd ; i++){
             pageNumber.push(i);
         }
-  }
+    }
+
+    useEffect(()=>{
+        const startIndex = Math.floor((currentPage-1)/limit)*limit;
+        setCurrentPageArray(pageNumber.slice(startIndex,startIndex+limit));
+    }, [currentPage, pageInfo]);
 
     return (
         <div className="paging-container">
@@ -21,7 +29,7 @@ function Pagination({ currentPage, setCurrentPage, pageInfo, postList }) {
                     > &lt; 이전
                 </button>
             }&nbsp;&nbsp;
-            {pageNumber.map((num) => (
+            {currentPageArray.map((num) => (
             <div key={num} onClick={() => {setCurrentPage(num);
                 window.scrollTo(0,0);}}>
                 <button
